ca73084fc653cfe3296068d78a762a539bd912f7
Eduardo Vidal <evidal86@gmail.com>
2016-11-27 15:11:55 +0900

    refactored a little, simplified some methods

--- mangazine.py
+++ mangazine.py
@@ -1,10 +1,13 @@
-#!/usr/bin/env python
-
-import argparse
+#!/usr/bin/env python3
+import utils
+from utils import Source
+#from models import Manga
 
 
 def Main():
-
+    manga = utils.get_manga_info("saint-oniisan", Source.MANGAFOX)
+    #print('manga name: ', manga.name)
+    #utils.parse_url("saint-oniisan", 1, Source.MANGAFOX)
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,15 +1,23 @@
-# Utils module
+from models import Manga
+import requests
 
-API_BASE_URL = "https://doodle-manga-scraper.p.mashape.com"
-API_KEY = "Mz7D0J6UuGmshhNukoJLus4IEzcIp1vc7DpjsnDxy25VptIsAi"
-X_MASHAPE_KEY = "wrIpZCP7H2mshF7sHe18Vo7LfYQRp1pU0AajsnA0FHrI0zQlWk"
+__API_BASE_URL = "https://doodle-manga-scraper.p.mashape.com"
+__MANGA_BASE_URL = __API_BASE_URL + "/{siteid}/manga/{mangaid}/{chapterid}"
+__API_KEY = "Mz7D0J6UuGmshhNukoJLus4IEzcIp1vc7DpjsnDxy25VptIsAi"
+__X_MASHAPE_KEY = "wrIpZCP7H2mshF7sHe18Vo7LfYQRp1pU0AajsnA0FHrI0zQlWk"
 
-def get_manga_info(manga_id, site):
-    url = API_BASE_URL + "/" + site + "/manga/" + manga_id
-    print("Calling the URL: " + url)
+class Source:
+    MANGAFOX = "mangafox.me"
+    MANGASTREAM = "mangastream.com"
+    MANGAREADER = "mangareader.net"
+
+def get_manga_info(manga_id, source):
+    url = parse_url(manga_id, "", source)
+    #url = API_BASE_URL + "/" + source + "/manga/" + manga_id
+    #print("Calling the URL: " + url)
     response = requests.get(url,
       headers={
-        "X-Mashape-Key": X_MASHAPE_KEY,
+        "X-Mashape-Key": __X_MASHAPE_KEY,
         "Accept": "text/plain"
       }
     )
@@ -18,3 +26,12 @@ def get_manga_info(manga_id, site):
 
     manga = Manga.from_json(response.json())
     return manga
+
+def parse_url(manga_id, chapter, source):
+    url = __MANGA_BASE_URL.replace("{mangaid}", manga_id)
+    url = url.replace("{siteid}", source)
+    url = url.replace("{chapterid}", str(chapter))
+    return url
+
+def get_chapter_info(manga_id, chapter, source):
+    url = parse_url(manga_id, chapter, source)
